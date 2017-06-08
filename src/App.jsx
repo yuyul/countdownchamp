249fb53aab8dfaa512f4d447c68644ecafb479dc
Juan Julian Caro Sancho <Juan Julian Caro Sancho>
2017-06-08 07:25:20 +0200

    Add react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
 import Clock from './Clock';
 import './App.css';
 
@@ -24,13 +25,14 @@ class App extends Component {
             <Clock 
                 deadline={this.state.deadline}
             />
-            <div>
-                <input 
+            <Form inline>
+                <FormControl 
+                    className='Deadline-input'
                     onChange={event => this.setState({newDeadline: event.target.value})}
                     placeholder='new date' 
                 />
-                <button onClick={() => this.changeDeadline()}>Submit</button>
-            </div>
+                <Button onClick={() => this.changeDeadline()}>Submit</Button>
+            </Form>
         </div>
         )
     }
